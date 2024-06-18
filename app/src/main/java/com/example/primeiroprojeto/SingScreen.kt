c4318bfa638a027325ab3e7af51d1705e7e8c76f
Joaosa100 <joaovitorsa100@gmail.com>
2024-06-18 01:56:18 -0300

    tentando recuperar as duas telas do rebase interativo

--- app/src/main/java/com/example/primeiroprojeto/SingScreen.kt
+++ app/src/main/java/com/example/primeiroprojeto/SingScreen.kt
@@ -1,21 +1,23 @@
 package com.example.primeiroprojeto
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxColors
-import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
@@ -35,13 +37,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.primeiroprojeto.ui.theme.PrimeiroProjetoTheme
 import com.example.primeiroprojeto.ui.theme.grayPrimary
 import com.example.primeiroprojeto.ui.theme.greenPrimary
@@ -56,6 +61,7 @@ fun SignScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .systemBarsPadding()
                 .padding(8.dp)
         ) {
@@ -64,6 +70,7 @@ fun SignScreen() {
             CustomSignInput("Email")
             CustomSignInputPassword("Password")
             CustomCheckBox()
+            SignUpButton()
         }
     }
 }
@@ -98,7 +105,8 @@ fun SignScreenTopAppBar() {
         Text(
             text = stringResource(id = R.string.sign_screen_topbar_name),
             color = Color.Black,
-            fontFamily = CustomFontFamily500
+            fontFamily = CustomFontFamily500,
+            fontSize = 24.sp,
         )
 
         ClickableText(
@@ -131,7 +139,9 @@ fun CustomSignInput(string: String) {
     }
 
     OutlinedTextField(
-        modifier = Modifier.fillMaxWidth().padding(top = 10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp),
         shape = RoundedCornerShape(10.dp),
         colors = TextFieldDefaults.outlinedTextFieldColors(
             unfocusedBorderColor = inputBorderColor,
@@ -146,6 +156,7 @@ fun CustomSignInput(string: String) {
                 color = grayPrimary
             )
         },
+        singleLine = true
     )
 }
 
@@ -157,7 +168,9 @@ fun CustomSignInputPassword(string: String) {
 
     OutlinedTextField(
         shape = RoundedCornerShape(10.dp),
-        modifier = Modifier.fillMaxWidth().padding(top = 10.dp, bottom = 10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp, bottom = 10.dp),
         colors = TextFieldDefaults.outlinedTextFieldColors(
             unfocusedBorderColor = inputBorderColor,
             focusedBorderColor = inputBorderColor,
@@ -171,6 +184,7 @@ fun CustomSignInputPassword(string: String) {
                 color = grayPrimary
             )
         },
+        singleLine = true,
         trailingIcon = {
             ClickableText(
                 modifier = Modifier.padding(end = 15.dp),
@@ -193,19 +207,57 @@ fun CustomCheckBox() {
     Row(
         modifier = Modifier.padding(top = 15.dp, bottom = 15.dp)
     ) {
-        Checkbox(checked = !checked,
+        Checkbox(
+            checked = !checked,
             onCheckedChange = { checked = it },
         )
         /*colors = CheckboxColors(
             //TODO
         )
         */
-        Text(modifier = Modifier,
-            text = stringResource(id = R.string.sign_screen_check_box),)
+        Text(
+            modifier = Modifier,
+            text = stringResource(id = R.string.sign_screen_check_box),
+        )
     }
 }
 
+@Composable
+fun SignUpButton() {
+    Column (
+        modifier = Modifier.padding(top = 20.dp, bottom = 10.dp)
+    ) {
+        Button(
+            modifier = Modifier
+                .height(51.dp)
+                .fillMaxWidth()
+                .padding(bottom = 10.dp),
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = greenPrimary,
+                contentColor = Color.White
+            ),
+        ) {
+            Text(text = stringResource(id = R.string.sign_screen_topbar_name))
+        }
+        ClickableText(
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .fillMaxWidth(),
+            text = AnnotatedString(
+                stringResource(
+                    id = R.string.sign_screen_lost)
+            ),
+            style = TextStyle(
+                color = greenPrimary,
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center
+            )
+        ) {
 
+        }
+    }
+}
 
 
 //TODO: FAZER CHECKBOX STYLE AND FINISH sign screen, fazer aulas screen
--- app/src/main/java/com/example/primeiroprojeto/ui/theme/Color.kt
+++ app/src/main/java/com/example/primeiroprojeto/ui/theme/Color.kt
@@ -10,6 +10,10 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8e266de (add undone sign up screen)
 val greenPrimary = Color(0xFF5DB075)
 val grayPrimary = Color(0xFFBDBDBD)
 val grayStringGCheckBox = Color(0xFF666666)
