3eb2b4a96e7efb6beb8d43b1028e319a66262426
Davi <davicalo2d@gmail.com>
2024-06-19 09:50:50 -0300

    finish

--- app/src/main/java/com/example/primeiroprojeto/chatScreen.kt
+++ app/src/main/java/com/example/primeiroprojeto/chatScreen.kt
@@ -1,23 +1,37 @@
-package com.example.primeiroprojeto.com.example.primeiroprojeto.ui.theme
+package com.example.primeiroprojeto
 
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -30,8 +44,8 @@ import com.example.primeiroprojeto.R
 @Composable
 fun chatScreen(){
     Scaffold(
-    topBar = { Header("Chat") },
-    bottomBar = {  },
+    topBar = { Header("Suporte") },
+    bottomBar = { inputScreenBottomBar() },
     ) { innerPadding ->
         Column(
             modifier = Modifier
@@ -39,18 +53,111 @@ fun chatScreen(){
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-
+            messagesComposable()
         }
     }
 }
 
-
-
-
-
+@Composable
+fun messagesComposable(){
+    Column(
+        Modifier.padding(20.dp)
+    ) {
+        repeat(2){
+            messageCard("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+        }
+        repeat(2){
+            messageCardAnother("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+        }
+    }
+}
 
 @Composable
-fun Header(Title: String){
+fun messageCard(Message: String){
+    val interMedium = FontFamily(Font(R.font.inter_medium, FontWeight.Medium))
+    Column(
+        Modifier.fillMaxWidth()
+            .padding(0.dp,5.dp),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(modifier = Modifier
+            .clip(
+            RoundedCornerShape(
+                topStart = 48f,
+                topEnd = 48f,
+                bottomStart = 48f ,
+                bottomEnd = 0f)
+            )
+            .background(Color(0xff5DB075))
+            .width(265.dp)
+            .padding(15.dp)
+        ) {
+            Text(text = Message, fontSize = 15.sp, fontFamily = interMedium, color = Color(0xFFFFFFFF))
+        }
+    }
+}
+
+@Composable
+fun messageCardAnother(Message: String){
+        val interMedium = FontFamily(Font(R.font.inter_medium, FontWeight.Medium))
+        Column(
+            Modifier.fillMaxWidth()
+                .padding(0.dp,5.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
+        ) {
+        Row(modifier = Modifier
+            .clip(
+                RoundedCornerShape(
+                    topStart = 48f,
+                    topEnd = 48f,
+                    bottomStart = 0f ,
+                    bottomEnd = 48f)
+            )
+            .background(Color(0xffC4C4C4))
+            .width(265.dp)
+            .padding(15.dp)
+        ) {
+            Text(text = Message, fontSize = 15.sp, fontFamily = interMedium, color = Color(0xFF000000))
+        }
+}}
+
+@Composable
+fun inputScreenBottomBar(){
+    var asd = ""
+    val interMedium = FontFamily(Font(R.font.inter_medium, FontWeight.SemiBold))
+    Row {
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 4.dp, bottom = 16.dp, start = 8.dp, end = 8.dp),
+            shape = RoundedCornerShape(50.dp),
+            value = asd ,
+            onValueChange = { asd = it },
+            label = {
+                Text(
+                    text = "Message here...",
+                    fontFamily = interMedium,
+                    fontSize = 16.sp,
+                    color = Color(0xffBDBDBD)
+                )
+            },
+            trailingIcon = {
+//                   IconButton(onClick = { /*TODO*/ }) {}
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.ArrowForward,
+                        contentDescription = null,
+                        tint = Color(0xff5DB075))
+                }
+            },
+            singleLine = true
+        )
+    }
+}
+
+@Composable
+fun Header(title: String){
     val interSemiBlod = FontFamily(Font(R.font.inter_semibold, FontWeight.SemiBold))
     val interMedium = FontFamily(Font(R.font.inter_medium, FontWeight.SemiBold))
     Row(
@@ -66,7 +173,7 @@ fun Header(Title: String){
             Text("Back", color = Color(0xff5DB075), fontSize = 16.sp, fontFamily = interMedium)
         }
 
-        Text(text = Title, fontFamily = interSemiBlod, fontWeight = FontWeight.SemiBold, fontSize = 30.sp )
+        Text(text = title, fontFamily = interSemiBlod, fontWeight = FontWeight.SemiBold, fontSize = 30.sp )
 
         Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color(0xfffffff))
         ) {
